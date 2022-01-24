88599188b8925c0bab3f2b2d8529c6ecf2434223
OmranK <o.khoja93@gmail.com>
2022-01-24 02:41:37 -0500

    Defines the FeedItem Model.

---
+++ FeedModule/FeedModule/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedModule
+//
+//  Created by Omran Khoja on 1/24/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
