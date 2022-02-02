55b4e3790855887e39256e02b9736e8add530baa
OmranK <o.khoja93@gmail.com>
2022-02-02 04:08:39 -0500

    Refactors to improve readability with a method.

--- FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
+++ FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
@@ -45,7 +45,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         let clientError = NSError(domain: "Test", code: 0)
         // Call
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -69,6 +69,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index ](error)
         }
     }
     
