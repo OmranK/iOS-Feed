ddcabfd0c25e85ba6e1c5ea9f34bb04a87dbd2c3
OmranK <o.khoja93@gmail.com>
2022-02-02 04:06:44 -0500

    Refactors to replace stubbing behavior with a completion capture and invocation.

--- FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
+++ FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
@@ -38,10 +38,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        // Stub an error so we can test this behavvior.
-        client.error = NSError(domain: "Test", code: 0)
+       
         var capturedErrors = [RemoteFeedLoader.Error]()
+        // Save
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        // Call
+        client.completions[0](clientError)
+        
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -58,12 +63,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
+        
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
