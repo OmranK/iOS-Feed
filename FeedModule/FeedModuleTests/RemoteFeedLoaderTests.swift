60757c338344a909fd29cda1ce38d3e9fe4f60b3
OmranK <o.khoja93@gmail.com>
2022-02-04 21:16:10 -0500

    Passes test - Delivers empty items array on 200 HTTP response with empty JSON list.

--- FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
+++ FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
@@ -66,6 +66,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -108,6 +120,3 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
 }
-
-
-// HOW DO WE KNOW THE JSON IS RLY INVALID?
