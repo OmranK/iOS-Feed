199cbf8f1b34d5bc224227f37513551819068756
OmranK <o.khoja93@gmail.com>
2022-02-07 23:55:48 -0500

    Creates a new target for end to end tests with a mock backend.

---
+++ FeedModule/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedAPIEndToEndTests.swift
+//  FeedAPIEndToEndTests
+//
+//  Created by Omran Khoja on 2/7/22.
+//
+
+import XCTest
+import FeedModule
+
+class FeedAPIEndToEndTests: XCTestCase {
+
+
+}
