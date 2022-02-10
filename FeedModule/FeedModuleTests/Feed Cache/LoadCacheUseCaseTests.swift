f02959e652d5dc75fa188e035569beb38f0276ee
OmranK <o.khoja93@gmail.com>
2022-02-10 00:51:23 -0500

    Passes test - LocalFeedLoader load command does not cache if cache is already empty.

--- FeedModule/FeedModuleTests/Feed Cache/LoadCacheUseCaseTests.swift
+++ FeedModule/FeedModuleTests/Feed Cache/LoadCacheUseCaseTests.swift
@@ -78,12 +78,21 @@ class LoadCacheUseCaseTests: XCTestCase {
     }
     
     func test_load_deletesCacheOnRetrievalError() {
-    
         let (sut, store) = makeSUT()
+        
         sut.load { _ in }
         store.completeRetrieval(with: anyNSError())
         
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
+    func test_load_doesNotDeleteCacheOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
     
