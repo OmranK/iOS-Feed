cdead6d91dfc6d3a868808de235992671c18a8ba
OmranK <o.khoja93@gmail.com>
2022-02-10 16:59:45 -0500

    Extracts hardcoded store URL from the CodableFeedStore production type making it an explicit dependency passed in through initializer injection.

--- FeedModule/FeedModuleTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedModule/FeedModuleTests/Feed Cache/CodableFeedStoreTests.swift
@@ -38,7 +38,11 @@ class CodableFeedStore {
         }
     }
     
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
     
     func retrieve(completion: @escaping FeedStore.RetrieveCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
@@ -139,7 +143,9 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore()
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+        let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
         
