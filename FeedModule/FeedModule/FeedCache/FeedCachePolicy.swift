fae53afbf901450ac794e6fa63945463859e07a7
OmranK <o.khoja93@gmail.com>
2022-02-10 12:37:50 -0500

    Moves FeedCachePolicy to its own file.

---
+++ FeedModule/FeedModule/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  FeedModule
+//
+//  Created by Omran Khoja on 2/10/22.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, from currentDate: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return currentDate < maxCacheAge
+    }
+}
