40ffbaa0864792fb5bf62c584664c09f658b684f
OmranK <o.khoja93@gmail.com>
2022-02-09 20:32:50 -0500

    Moves RemoteFeedItem and LocalFeedItem DTO models to their own files.

--- FeedModule/FeedModule/FeedAPI/FeedItemsMapper.swift
+++ FeedModule/FeedModule/FeedAPI/FeedItemsMapper.swift
@@ -23,10 +23,3 @@ internal final class FeedItemsMapper {
         return root.items
     }
 }
-
-internal struct RemoteFeedItem: Codable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-}
---
+++ FeedModule/FeedModule/FeedAPI/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  FeedModule
+//
+//  Created by Omran Khoja on 2/9/22.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Codable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
