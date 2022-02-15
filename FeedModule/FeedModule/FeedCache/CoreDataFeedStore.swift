3b2ea12054ff02ad17fbbe4256dccdc08c1b4066
OmranK <o.khoja93@gmail.com>
2022-02-15 14:48:41 -0500

    Passes test - `CoreDataFeedStore.retrieve()` delivers found values on non empty cache.

--- FeedModule/FeedModule/FeedCache/CoreDataFeedStore.swift
+++ FeedModule/FeedModule/FeedCache/CoreDataFeedStore.swift
@@ -22,13 +22,51 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
+        context.perform { [context] in
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = NSOrderedSet(array: feed.map { localFeedImage in
+                    let managedFeedImage = ManagedFeedImage(context: context)
+                    managedFeedImage.id = localFeedImage.id
+                    managedFeedImage.imageDescription = localFeedImage.description
+                    managedFeedImage.location = localFeedImage.location
+                    managedFeedImage.url = localFeedImage.url
+                    return managedFeedImage
+                })
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        context.perform { [context] in
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    completion(.found(
+                        feed: cache.feed
+                            .compactMap { $0 as? ManagedFeedImage }
+                            .map {
+                                LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                            },
+                        timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
+    
 }
+
+// MARK: - Helper Extensions
 
 private extension NSPersistentContainer {
     enum LoadingError: Swift.Error {
@@ -59,12 +97,14 @@ private extension NSManagedObjectModel {
     }
 }
 
-
+// MARK: - Core Data object models
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
 
+@objc(ManagedFeedImage)
 private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
