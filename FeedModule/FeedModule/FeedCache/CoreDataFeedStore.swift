73aed1ad063e4d4e83dd3cb7d53d323effba5221
OmranK <o.khoja93@gmail.com>
2022-02-15 13:50:57 -0500

    Adds private background context for `NSPersistentContainer`'s `NSManagedStoreCoordinator` to perform store operations on

--- FeedModule/FeedModule/FeedCache/CoreDataFeedStore.swift
+++ FeedModule/FeedModule/FeedCache/CoreDataFeedStore.swift
@@ -10,9 +10,11 @@ import CoreData
 public final class CoreDataFeedStore: FeedStore {
     
     private let persistentContainer: NSPersistentContainer
+    private let context: NSManagedObjectContext
     
     public init(bundle: Bundle = .main) throws {
         persistentContainer = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+        context = persistentContainer.newBackgroundContext()
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
