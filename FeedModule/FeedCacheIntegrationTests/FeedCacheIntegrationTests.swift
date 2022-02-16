e1fb456090e279d7f300ec06cde93362a53b6a97
OmranK <o.khoja93@gmail.com>
2022-02-15 21:52:49 -0500

    Adds `FeedCacheIntegrationTests` target for the slower cache-integration tests and create a separation from the faster unit tests.

---
+++ FeedModule/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedCacheIntegrationTests.swift
+//  FeedCacheIntegrationTests
+//
+//  Created by Omran Khoja on 2/15/22.
+//
+
+import XCTest
+
+class FeedCacheIntegrationTests: XCTestCase {
+    
+    
+
+}
