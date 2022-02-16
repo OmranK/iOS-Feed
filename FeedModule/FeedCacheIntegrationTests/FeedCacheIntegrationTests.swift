65d930da64675981d8335c0506d0314f211a5fe3
OmranK <o.khoja93@gmail.com>
2022-02-15 22:37:57 -0500

    Adds store artifacts cleanup to undo all cache side effects via `setUp` and `tearDown` to avoid sharing state between tests.

--- FeedModule/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
+++ FeedModule/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -10,6 +10,16 @@ import FeedModule
 
 class FeedCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -42,6 +52,18 @@ class FeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
 }
