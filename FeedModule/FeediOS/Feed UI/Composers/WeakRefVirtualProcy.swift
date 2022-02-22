731db83367c7fc7f3a373c4288d0f4ca28e6c937
OmranK <o.khoja93@gmail.com>
2022-02-22 04:15:02 -0500

    Organizes Composition layer components into separate files.

---
+++ FeedModule/FeediOS/Feed UI/Composers/WeakRefVirtualProcy.swift
@@ -0,0 +1,30 @@
+//
+//  WeakRefVirtualProcy.swift
+//  FeediOS
+//
+//  Created by Omran Khoja on 2/22/22.
+//
+
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T:FeedImageView, T.Image == UIImage {
+    typealias Image = T.Image
+    
+    func display(_ model: ImageViewModel<Image>) {
+        object?.display(model)
+    }
+}
