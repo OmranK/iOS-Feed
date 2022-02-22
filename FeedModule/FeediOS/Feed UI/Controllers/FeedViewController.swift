e13ac545217a33a6032ea31baee2fec7ba67e208
OmranK <o.khoja93@gmail.com>
2022-02-22 01:15:53 -0500

    Moves title configuration form the `FeedViewController` to the `FeedUICompose` - so the View Controller stays agnostic from the Presenter

--- FeedModule/FeediOS/Feed UI/Controllers/FeedViewController.swift
+++ FeedModule/FeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -24,7 +24,6 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         refresh()
-        title = FeedPresenter.title
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
