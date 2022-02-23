1a42162506875ff785a14e29ff70f6e4a4ba03bb
OmranK <o.khoja93@gmail.com>
2022-02-23 11:32:42 -0500

    Passes test - `FeedPresenter` does not send any messages to the view on init.

---
+++ FeedModules/Presentation Layer/FeedPresentationModuleTests/FeedPresenterTests.swift
@@ -0,0 +1,39 @@
+//
+//  FeedPresenterTests.swift
+//  FeedCoreModule
+//
+//  Created by Omran Khoja on 2/23/22.
+//
+
+import XCTest
+
+final class FeedPresenter {
+    init(view: Any) {
+        
+    }
+}
+
+class FeedPresenterTests: XCTestCase {
+    
+    func test_init_doesNotSendMessagesToView() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let spy = ViewSpy()
+        let sut = FeedPresenter(view: spy)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(spy, file: file, line: line)
+        return (sut, spy)
+    }
+    
+    private class ViewSpy {
+        var messages = [Any]()
+    }
+
+
+}
