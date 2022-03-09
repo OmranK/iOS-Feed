5e07b7bb9344a3a89d7bc9ea14cbecb75702920c
OmranK <o.khoja93@gmail.com>
2022-03-09 14:59:30 -0500

    Passes test - FeedPresenter title is localized string.

--- FeedModules/Presentation Layer/FeedPresentationModuleTests/FeedPresenterTests.swift
+++ FeedModules/Presentation Layer/FeedPresentationModuleTests/FeedPresenterTests.swift
@@ -45,6 +45,13 @@ final class FeedPresenter {
     let loadingView: FeedLoadingView
     let errorView: FeedLoadingErrorView
     let feedView: FeedView
+    
+    static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for the feed view")
+    }
 
     init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedLoadingErrorView) {
         self.loadingView = loadingView
@@ -73,6 +80,10 @@ final class FeedPresenter {
 }
 
 class FeedPresenterTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
     
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
