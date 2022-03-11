0f59e71daddaaf0263ff90632431cce86a2092ae
OmranK <o.khoja93@gmail.com>
2022-03-10 21:01:16 -0500

    `LocalImageLoader` saveImageDataForURL requests image data insertion into store

--- FeedModules/Core Layer/Tests/FeedCoreModuleTests/Feed Cache/Helpers/ImageDataStoreSpy.swift
+++ FeedModules/Core Layer/Tests/FeedCoreModuleTests/Feed Cache/Helpers/ImageDataStoreSpy.swift
@@ -14,8 +14,9 @@ class ImageDataStoreSpy: ImageDataStore {
         case retrieve(dataFor: URL)
     }
     
-    private var retrievalCompletions = [(ImageDataStore.RetrievalResult) -> Void]()
     private(set) var receivedMessages = [Message]()
+    private var retrievalCompletions = [(ImageDataStore.RetrievalResult) -> Void]()
+    private var insertionCompletions = [(ImageDataStore.InsertionResult) -> Void]()
     
     func retrieve(dataForURL url: URL, completion: @escaping (ImageDataStore.RetrievalResult) -> Void) {
         receivedMessages.append(.retrieve(dataFor: url))
@@ -24,6 +25,7 @@ class ImageDataStoreSpy: ImageDataStore {
     
     func insert(_ data: Data, for url: URL, completion: @escaping (ImageDataStore.InsertionResult) -> Void) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
@@ -32,5 +34,9 @@ class ImageDataStoreSpy: ImageDataStore {
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
