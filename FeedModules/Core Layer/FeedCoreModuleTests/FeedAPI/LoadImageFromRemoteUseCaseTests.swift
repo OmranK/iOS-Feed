bf0f946f472d86feb700ad538d859dd2ac141583
OmranK <o.khoja93@gmail.com>
2022-03-10 19:58:57 -0500

    Moves `RemoteFeedImageDataLoader` to production

--- FeedModules/Core Layer/FeedCoreModuleTests/FeedAPI/LoadImageFromRemoteUseCaseTests.swift
+++ FeedModules/Core Layer/FeedCoreModuleTests/FeedAPI/LoadImageFromRemoteUseCaseTests.swift
@@ -8,59 +8,6 @@
 import XCTest
 import FeedCoreModule
 
-class RemoteImageLoader {
-    
-    let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-    
-    private final class HTTPClientTaskWrapper: ImageLoaderTask {
-        private var completion: ((ImageLoader.Result) -> Void)?
-        
-        var wrapped: HTTPClientTask?
-        
-        init(_ completion: @escaping (ImageLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: ImageLoader.Result) {
-            completion?(result)
-        }
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (ImageLoader.Result) -> Void) -> ImageLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(error))
-            }
-        }
-        return task
-    }
-}
-
 class LoadImageFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
