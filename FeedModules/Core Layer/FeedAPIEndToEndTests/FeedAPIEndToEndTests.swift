5623852f2b0bdc4be7cdf4c24cce6e557d3347a9
OmranK <o.khoja93@gmail.com>
2022-03-10 20:12:27 -0500

    Extract duplicate ephemeral HTTPClient creation into a factory helper

--- FeedModules/Core Layer/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ FeedModules/Core Layer/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -63,13 +63,11 @@ class FeedAPIEndToEndTests: XCTestCase {
     }
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> ImageLoader.Result? {
-        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
-        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteImageLoader(client: client)
-        trackForMemoryLeaks(client, file: file, line: line)
+        let loader = RemoteImageLoader(client: ephemeralClient())
         trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         
         var receivedResult: ImageLoader.Result?
         _ = loader.loadImageData(from: url) { result in
@@ -83,6 +81,12 @@ class FeedAPIEndToEndTests: XCTestCase {
     
     private var feedTestServerURL: URL {
         return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+    }
+    
+    private func ephemeralClient(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        trackForMemoryLeaks(client, file: file, line: line)
+        return client
     }
     
     private func expectedImage(at index: Int) -> FeedImage {
