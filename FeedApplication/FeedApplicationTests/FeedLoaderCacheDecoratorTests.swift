d4a4a86117877f6978251cf151a862c0fa593a03
OmranK <o.khoja93@gmail.com>
2022-03-13 16:46:25 -0400

    `FeedLoaderCacheDecorator` delivers decorated loader result.

---
+++ FeedApplication/FeedApplicationTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,79 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  FeedApplicationTests
+//
+//  Created by Omran Khoja on 3/13/22.
+//
+
+import FeedCoreModule
+import XCTest
+
+class FeedLoaderCacheDecorator: FeedLoader {
+    
+    let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+class FeedLoaderCacheDecoratorTests: XCTestCase {
+    
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueFeed()
+        let loader = LoaderStub(result: .success(feed))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, toCompleteWith: .success(feed))
+    }
+    
+    func test_load_deliversErrorOnLoaderError() {
+        let error = anyNSError()
+        let loader = LoaderStub(result: .failure(error))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, toCompleteWith: .failure(error))
+    }
+    
+    // MARK: - Helpers
+    
+    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load() { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedImageData), .success(expectedImageData)):
+                XCTAssertEqual(receivedImageData, expectedImageData, file: file, line: line)
+            case (.failure, .failure):
+                break
+            default:
+                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 3.0)
+    }
+    
+    private func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any description", location: "any location", url: anyURL())]
+    }
+    
+    // MARK: - Stub
+    
+    private class LoaderStub: FeedLoader {
+        
+        let result: FeedLoader.Result
+        
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+}
