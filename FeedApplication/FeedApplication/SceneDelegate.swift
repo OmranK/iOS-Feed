29d6be124907ed3b189cdc88d598217b82f3a682
OmranK <o.khoja93@gmail.com>
2022-03-14 18:18:31 -0400

    Passes test - displays cached feed on launch when customer has no connectivity.
    Test requires complicated simulation of an application launch with no connectivity.

--- FeedApplication/FeedApplication/SceneDelegate.swift
+++ FeedApplication/FeedApplication/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 import FeedCoreModule
 import FeediOS
 
@@ -17,15 +18,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
+        let client = makeRemoteClient()
         let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
         let remoteImageLoader = RemoteImageLoader(client: client)
         
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        let localImageLoader = LocalImageLoader(store: localStore)
+        
         window?.rootViewController =
         FeedUIComposer.feedControllerComposedWith(
-            feedLoader: remoteFeedLoader,
-            imageLoader: remoteImageLoader)
+        feedLoader: FeedLoaderWithFallbackComposite(
+            primary: FeedLoaderCacheDecorator(
+                decoratee: remoteFeedLoader,
+                cache: localFeedLoader),
+            fallback: localFeedLoader),
+        imageLoader: ImageLoaderWithFallbackComposite(
+            primary: localImageLoader,
+            fallback: ImageLoaderCacheDecorator(
+                decoratee: remoteImageLoader,
+                cache: localImageLoader)))
+
+    }
+    
+    private func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            return AlwaysFailingHTTPClient()
+        default:
+            let session = URLSession(configuration: .ephemeral)
+            return URLSessionHTTPClient(session: session)
+        }
+    }
+}
+
+private class AlwaysFailingHTTPClient: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        completion(.failure(NSError(domain: "offline", code: 0)))
+        return Task()
     }
 }
 
